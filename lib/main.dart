030594aa84881a10058d52568b77887257275440
FarhaN400 <fa07204377@gmail.com>
2025-09-26 21:06:43 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,37 +10,116 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: 550,
-        height: 300,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image:AssetImage("assets/images/bg.png"),
-            fit: BoxFit.cover
+      body: Stack(
+        children: [
+          Container(
+          height: size.height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/bg.png"),
+              fit: BoxFit.cover,
+            ),
           ),
-          borderRadius: BorderRadius.circular(15)
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(Icons.arrow_back,color: Colors.black87,),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: [
+                Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        child: Icon(Icons.arrow_back, color: Colors.white),
+                        onTap: () {},
+                      ),
+                      SizedBox(width: size.width * .84),
+                      InkWell(
+                        child: Icon(Icons.menu, color: Colors.white),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Farhan Akhtar",
+                        style: TextStyle(color: Colors.white, fontSize: 28),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "UPI ID : fa0720436@oksbi",
+                        style: TextStyle(color: Colors.white70, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        '82XXXXXX92',
+                        style: TextStyle(color: Colors.white70, fontSize: 20),
+                      ),
+                      SizedBox(width: 18,),
+                      Container(
+                        width: 155,
+                        height: 28,
+                        decoration: BoxDecoration(
+                          color: Colors.black87,
+                          borderRadius: BorderRadius.circular(16)
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.add_circle,color: Colors.lightBlue,),
+                            Text(
+                              "Link UPI Number",
+                              style: TextStyle(
+                                color: Colors.lightBlue
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 45,
+                        backgroundImage: AssetImage('assets/images/Farhan.png'),
+                      )
+                    ],
+                  ),
+                )
+              ]
+            ),
+          ),
 
-            ],
           ),
-        ),
+        ],
       ),
     );
   }
